dc49472e31479c3acc749fea7c0e050c8b5c4a69
MohamadAhmadi <mohammadahmadidezhnet@gmail.com>
2021-04-19 14:08:35 +0430

    changed settings.py

--- DjangoBlogProject/settings.py
+++ DjangoBlogProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_ulvz$5yqf7$h80q0r7%fkgpf7$-75dn!vwt-dlnu6pr8@u3jx'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
